3d72439ba38ef14af768808e2573564a6fb65035
Aiden-Patterson <97368066+Aiden-Patterson@users.noreply.github.com>
2024-01-08 13:41:00 -0700

    Update pokemon.js
    added newer generations

--- scripts/pokemon.js
+++ scripts/pokemon.js
@@ -1,53 +1,37 @@
-const output = (pokemon) => {
-  let gameName = document.createElement("h2");
-  gameName.textContent = "Game: " + pokemon.name;
-  document.querySelector("#gameName").textContent = "Game: " + pokemon.name;
-  for(let i = 1; i <= 6; i++)
-  {
-      let rand = Math.floor(Math.random() * pokemon.pokemon_entries.length);
-      let pokemonName = document.createElement("h3");
-      pokemonName.textContent = "Name: " + pokemon.pokemon_entries[rand].pokemon_species.name;
-  
-      let pokedex = document.createElement("h4");
-      //pokedex.textContent = "Pokedex: " + pokemon.id;
-  
-      let type1 = document.createElement("h4");
-     // type1.textContent = "Type 1: " + pokemon.types[0].type.name;
-      let type2 = document.createElement("h4");
-      //if(pokemon.types.length == 2)
-      {
-        //type2.textContent = "Type 2: " + pokemon.types[1].type.name;
-      }
-      //else
-      {
-        type2.textContent = "Type 2: N/A";
-      }
-  
-      let sprite = document.createElement("img")
-      //sprite.setAttribute("src", pokemon.sprites.front_default);
-      //sprite.setAttribute("alt", pokemon.templeName);
-  
-      article = document.createElement("article");
-      article.appendChild(pokemonName);
-      //article.appendChild(pokedex);
-      //article.appendChild(type1);
-      //article.appendChild(type2);
-      //article.appendChild(sprite);
-  
-      document.querySelector("#pokemon").appendChild(article);
-    }
-  };
+<!DOCTYPE html>
+<html lang="en">
 
-  const reset = () => {
-    document.querySelector('#pokemon').innerHTML = '';
-  }
-  generation = document.querySelector('#games')
-async function getPokemon(){
-  reset();
-    //const result = await fetch('https://pokeapi.co/api/v2/pokemon/' + rand_id);
-    const result = await fetch('https://pokeapi.co/api/v2/pokedex/' + generation.value)
-    const data = await result.json();
-    output(data);
-}
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>CSE 121b: Week 05 | Sample Solution</title>
+    <link href="https://fonts.googleapis.com/css?family=Kalam&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="styles/main.css">
+</head>
+<body>
+    <h1>Create your own team!</h1>
+    <label for="generation">Choose a generation:</label>
 
-document.querySelector("#randomPokemon").addEventListener("click", getPokemon);
+<select name="games" id="games">
+    <option value="1">National</option>
+  <option value="2">Kanto</option>
+  <option value="3">Johto</option>
+  <option value="4">Hoenn</option>
+  <option value="5">Sinnoh</option>
+  <option value="6">Sinnoh v2</option>
+  <option value="7">Johto v2</option>
+  <option value="8">Unova</option>
+  <option value="9">Unova v2</option>
+  <option value="27">Galar</option>
+  <option value="28">Isle of Armor</option>
+  <option value="29">Crown Tundra</option>
+  <option value="30">Hisui</option>
+  <option value="31">Paldea</option>
+</select>
+    <button id="randomPokemon">Generate Random Pokemon</button>
+    <div id="gameName"></div>
+    <div id="pokemon">
+    </div> 
+    <script src="scripts/pokemon.js"></script>
+</body>
